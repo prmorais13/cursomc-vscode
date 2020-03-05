884b5f692a154980e6168b0099d646358027f410
Paulo Roberto <prmorais_13@hotmail.com>
2020-03-05 20:57:28 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/com/pgm/cursomc/cursomc/domain/Categoria.java
@@ -0,0 +1,21 @@
+package com.pgm.cursomc.cursomc.domain;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+}
--- src/main/java/com/pgm/cursomc/cursomc/resources/CategoriasResource.java
+++ src/main/java/com/pgm/cursomc/cursomc/resources/CategoriasResource.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pgm.cursomc.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriasResource {
 
 	@GetMapping()
-	public String listar() {
-		return "REST api funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
